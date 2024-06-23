247e78cd8aeb06f7d40cbb802d6fe97aa6199558
0xA54 <141027176+0xA54@users.noreply.github.com>
2024-06-23 14:30:25 +1000

    Implemented XO_RCO_TEST register

---
+++ src/registers/xo_rco_test.rs
@@ -0,0 +1,17 @@
+use register_rs::*;
+
+#[derive(New, Register, ReadableRegister, WriteableRegister)]
+#[register(address = 0xB4, length = 1)]
+/// `XO_RCO_TEST` register
+pub struct XoRcoTest {
+    /// Reserved
+    #[register(bits = "4..7", reset = 0b0010)]
+    _reserved_0: u8,
+    /// `true`: disable both dividers of the digital clock (and reference 
+    /// clock for the SMPS) and IF-DC clock.    
+    #[register(bit = "3", reset = false)]
+    pub pd_clkdiv: bool,
+    /// Reserved
+    #[register(bits = "0..2", reset = 0b001)]
+    _reserved_1: u8,
+}
