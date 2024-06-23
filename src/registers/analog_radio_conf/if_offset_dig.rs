be5298eda11a012dd7c42a606ff365e529a10e9c
0xA54 <141027176+0xA54@users.noreply.github.com>
2024-06-23 14:30:29 +1000

    Implemented IF_OFFSET_DIG

---
+++ src/registers/analog_radio_conf/if_offset_dig.rs
@@ -0,0 +1,11 @@
+use register_rs::*;
+
+/// `IF_OFFSET_DIG` register
+#[derive(Register, ReadableRegister, WriteableRegister)]
+#[register(address = 0x0D, length = 1)]
+pub struct IfOffsetDig {
+    /// Intermediate frequency setting for the digital shift-to-baseband
+    /// (see Section 9.4)
+    #[register(bits = "0..7", reset = 0xA3)]
+    pub if_offset_dig: u8,
+}
