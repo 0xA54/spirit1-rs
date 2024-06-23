5219f1a9e746e962610ff0c0659f02542ae80cf2
0xA54 <141027176+0xA54@users.noreply.github.com>
2024-06-23 14:30:28 +1000

    Implemented CH_SPACE

---
+++ src/registers/analog_radio_conf/chspace.rs
@@ -0,0 +1,10 @@
+use register_rs::*;
+
+/// `CHSPACE` register
+#[derive(Register, ReadableRegister, WriteableRegister)]
+#[register(address = 0x0C, length = 1)]
+pub struct ChSpace {
+    /// Channel spacing in steps of `f_XO/215` (~793 for f_XO = 26 MHz, ~732 for f_XO = 24 MHz).
+    #[register(bits = "0..7", reset = 0xFC)]
+    pub ch_spacing: u8,
+}
