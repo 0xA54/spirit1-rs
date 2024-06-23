0897bbd2ec587171d13c720b312ca2740f4ac1c1
0xA54 <141027176+0xA54@users.noreply.github.com>
2024-06-23 14:30:33 +1000

    Implemented SYNCx registers

---
+++ src/registers/packet_protocol_conf/sync.rs
@@ -0,0 +1,37 @@
+use register_rs::*;
+
+/// `SYNC4` register
+#[derive(Register, ReadableRegister, WriteableRegister)]
+#[register(address = 0x36, length = 1)]
+pub struct Sync4 {
+    /// Sync word 4
+    #[register(bits = "0..7", reset = 0x88)]
+    pub sync4: u8,
+}
+
+/// `SYNC3` register
+#[derive(Register, ReadableRegister, WriteableRegister)]
+#[register(address = 0x37, length = 1)]
+pub struct Sync3 {
+    /// Sync word 3
+    #[register(bits = "0..7", reset = 0x88)]
+    pub sync3: u8,
+}
+
+/// `SYNC2` register
+#[derive(Register, ReadableRegister, WriteableRegister)]
+#[register(address = 0x38, length = 1)]
+pub struct Sync2 {
+    /// Sync word 2
+    #[register(bits = "0..7", reset = 0x88)]
+    pub sync2: u8,
+}
+
+/// `SYNC1` register
+#[derive(Register, ReadableRegister, WriteableRegister)]
+#[register(address = 0x39, length = 1)]
+pub struct Sync1 {
+    /// Sync word 1
+    #[register(bits = "0..7", reset = 0x88)]
+    pub sync1: u8,
+}
